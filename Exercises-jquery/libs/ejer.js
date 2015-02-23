cfcd9ea1af98a3d2731360e3dd663766d932fd7d
sapox <zapponi.juan.jose@gmail.com>
2015-02-23 18:13:30 -0300

    + jquery

---
+++ Exercises-jquery/libs/ejer.js
@@ -0,0 +1,15 @@
+//Select all of the div elements that have a class of "module".
+$(document).ready(function(){
+	var module = $('.module');
+	console.log(module);
+//Come up with three selectors that you could use 
+//to get the third item in the #myList unordered list
+	var lista = $('#myList');
+	var salida = lista.children();
+	console.log(salida[2]);
+	//$('#myList').('li:nth-last-child(2)');
+
+//Select the label for the search input using an attribute selector
+//
+
+});
