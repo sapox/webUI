72aa2b85f88bb744af4df017c3cfda2b97b857ba
sapox <zapponi.juan.jose@gmail.com>
2015-02-24 18:20:17 -0300

    ejercicios jquery

--- Exercises-jquery/libs/ejer.js
+++ Exercises-jquery/libs/ejer.js
@@ -21,8 +21,33 @@ $(document).ready(function(){
 	var images = $('img[alt]').length;
 //Select all of the odd table rows in the table body
 	$( "tr:odd" ).css( "background-color", "#ccccff" ); // y le aplico un color	
+
+//Select all of the image elements on the page
+//Log each image's alt attribute
 //con esto selecciono todas las imagenes
-	$('img');
-//con esto traigo el alt de una sola imagen
-	$('img').attr('alt');
+	var img = $('img');
+//con esto traigo el alt de todas
+	for (var a=0;a<img.length;a++){
+		console.log($('img').eq(a).attr('alt'));
+	}
+//Select the search input text box, then traverse up 
+//to the form and add a class to the form.
+	var input = $("input[type='text']");
+	input.parent().addClass("claseAgregada");
+//Select the list item inside #myList that has a class of "current"
+//Remove that class from it
+//Add a class of "current" to the next list item
+	var list = $('#myList > li.current');
+	list.removeClass("current").addClass("");
+	list.next().addClass("current");
+//Select the select element inside #specials
+//Traverse your way to the submit button.
+	var spe = $('#specials').children();
+	spe.parent().parent().children().children().next().eq(1);
+//Select the first list item in the #slideshow element
+//Add the class "current" to it, and then add a class 
+//of "disabled" to its sibling elements
+	var firstLi = $('#slideshow').children().eq(0);
+	firstLi.addClass("current");
+	firstLi.siblings().addClass("disabled");
 });
